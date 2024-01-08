5f84c21dd9305524d94d6801d95558a6b937eb13
ameliapham <ptt.huong.8895@gmail.com>
2024-01-08 22:53:00 +0100

    Add function and event for button element

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const input = document.querySelector('#input-task')
 input.value = null
 const button = document.querySelector('.add-task')
 const taskContainer = document.querySelector('.task-container')
+
+// Function
+const addTask = () => {
+    if (input.value != null){
+        const task = input.value
+        taskContainer.append(task)
+        console.log(task)
+    }
+}
+
+button.addEventListener('click', addTask)
