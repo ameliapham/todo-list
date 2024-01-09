a9fecb4d14d95751f669df0f66fffff123fe6e96
ameliapham <ptt.huong.8895@gmail.com>
2024-01-09 21:20:39 +0100

    Create function for Adding button

--- script.js
+++ script.js
@@ -3,16 +3,48 @@ import './style.css'
 //Variables
 const input = document.querySelector('#input-task')
 input.value = null
-const button = document.querySelector('.add-task')
+const addButton = document.querySelector('.add-task')
 const taskContainer = document.querySelector('.task-container')
 
 // Function
 const addTask = () => {
     if (input.value != null){
-        const task = input.value
-        taskContainer.append(task)
-        console.log(task)
+
+        // Add new-task zone
+        let newTask = document.createElement("div")
+        newTask.className = "new-task"
+        taskContainer.appendChild(newTask)
+
+        // Add tasks
+        let tasks = document.createElement("p")
+        tasks.className = "tasks"
+        tasks.innerHTML = input.value
+        newTask.append(tasks)
+
+        // Add buttons
+        let buttons = document.createElement("div")
+        buttons.className = "buttons"
+        newTask.appendChild(buttons)
+
+        let checkButton = document.createElement("button")
+        checkButton.className = "check-task"
+        let iconCheck = document.createElement("i")
+        iconCheck.className = "ri-check-line ri-lg"
+        checkButton.appendChild(iconCheck)
+
+        let deleteButton = document.createElement("button")
+        deleteButton.className = "delete-task"
+        let iconDelete = document.createElement("i")
+        iconCheck.className = "ri-delete-bin-6-line ri-lg"
+        deleteButton.appendChild(iconDelete)
+
+        buttons.appendChild(checkButton)
+        buttons.appendChild(deleteButton)
+        
+        // Reset value of input
+        input.value = null
     }
 }
 
-button.addEventListener('click', addTask)
+
+addButton.addEventListener('click', addTask)
