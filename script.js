89ae60f383fcac5a7d26ee315626dffb8936a913
ameliapham <ptt.huong.8895@gmail.com>
2024-01-09 21:41:25 +0100

    Fix function of Adding button

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 import './style.css'
 
-//Variables
+// Variables generals
 const input = document.querySelector('#input-task')
 input.value = null
-const addButton = document.querySelector('.add-task')
 const taskContainer = document.querySelector('.task-container')
 
-// Function
+let newTask = document.createElement("div")
+newTask.className = "new-task"
+
+let tasks = document.createElement("p")
+tasks.className = "tasks"
+
+let buttons = document.createElement("div")
+buttons.className = "buttons"
+
+// Variables for Adding button
+const addButton = document.querySelector('.add-task')
+
+// Variables for Checking button
+let checkButton = document.createElement("button")
+checkButton.className = "check-task"
+let iconCheck = document.createElement("i")
+iconCheck.className = "ri-check-line ri-lg"
+checkButton.appendChild(iconCheck)
+
+// Variables for Deleting button
+let deleteButton = document.createElement("button")
+deleteButton.className = "delete-task"
+let iconDelete = document.createElement("i")
+iconDelete.className = "ri-delete-bin-6-line ri-lg"
+deleteButton.appendChild(iconDelete)
+
+
+// Function for Adding button
 const addTask = () => {
     if (input.value != null){
 
         // Add new-task zone
-        let newTask = document.createElement("div")
-        newTask.className = "new-task"
         taskContainer.appendChild(newTask)
 
         // Add tasks
-        let tasks = document.createElement("p")
-        tasks.className = "tasks"
         tasks.innerHTML = input.value
         newTask.append(tasks)
 
         // Add buttons
-        let buttons = document.createElement("div")
-        buttons.className = "buttons"
         newTask.appendChild(buttons)
-
-        let checkButton = document.createElement("button")
-        checkButton.className = "check-task"
-        let iconCheck = document.createElement("i")
-        iconCheck.className = "ri-check-line ri-lg"
-        checkButton.appendChild(iconCheck)
-
-        let deleteButton = document.createElement("button")
-        deleteButton.className = "delete-task"
-        let iconDelete = document.createElement("i")
-        iconDelete.className = "ri-delete-bin-6-line ri-lg"
-        deleteButton.appendChild(iconDelete)
-
         buttons.appendChild(checkButton)
         buttons.appendChild(deleteButton)
         
@@ -48,3 +55,4 @@ const addTask = () => {
 
 
 addButton.addEventListener('click', addTask)
+
